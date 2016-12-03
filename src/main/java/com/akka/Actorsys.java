da79e024fce0bff374c2cc532e1a58ed5a089fd8
inoubliwissem <inoubliwissem@gmail.com>
2016-12-03 20:26:45 +0100

    first commit akka

--- src/main/java/com/akka/Actorsys.java
+++ src/main/java/com/akka/Actorsys.java
@@ -3,16 +3,22 @@ package com.akka;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import com.akka.Worker;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValueFactory;
+import java.io.FileReader;
+import java.util.Scanner;
 
-public class Actorsys{
+public class Actorsys {
 
-public static void main(String args[] )
-{
-System.out.println("hello system");
-ActorSystem system = ActorSystem.create("CalcSystem");
-//ActorRef worker = system.actorOf(Props.create(Worker.class), "master");
-//ActorRef worker = system.actorOf(new Props(Worker.class), "master");
-//final ActorRef worker = system.actorOf(Props.create(Worker.class), "worker");
-}
+    public static void main(String args[]) {
+        System.out.println("hello system");
 
+        ActorSystem system = ActorSystem.create("CalcSystem");
+        ActorRef worker = system.actorOf(Props.create(Worker.class), "agent1");
+        ActorRef worker2 = system.actorOf(Props.create(Worker2.class), "agent2");
+
+        worker.tell(new String("salut x"), worker);
+
+    }
 }
--- src/main/java/com/akka/Worker2.java
+++ src/main/java/com/akka/Worker2.java
@@ -1,19 +1,18 @@
 package com.akka;
 import akka.actor.ActorRef;
-import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
-public class Worker extends UntypedActor {
+public class Worker2 extends UntypedActor {
 
     LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
     public void onReceive(Object message) throws Exception {
-
         if (message instanceof String) {
-
-            System.out.println(message + "from" + getSender().toString());
+            // log.info("Received String message: {}", message);
+            getSender().tell(message, getSelf());
+            System.out.println(message);
 
         } else {
             unhandled(message);
